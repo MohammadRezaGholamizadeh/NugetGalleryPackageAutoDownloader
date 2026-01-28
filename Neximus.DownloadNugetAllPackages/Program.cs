d6cc0279a3d988dc902f155484963b3074fa5aea
Mohammadreza_Gholamizadeh <pranceoffire57@gmail.com>
2026-01-28 05:47:28 +0330

    added download count filter and sort for downloading

--- Neximus.DownloadNugetAllPackages/Program.cs
+++ Neximus.DownloadNugetAllPackages/Program.cs
@@ -1,83 +1,143 @@
 ﻿using NuGet.Common;
 using NuGet.Configuration;
+using NuGet.Packaging.Core;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+using System.Net.Http;
+using System.Text.Json;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        string rootFolder = "DownloadedPackages";
+        string rootFolder = @"D:\Downloaded Packages";
         Directory.CreateDirectory(rootFolder);
 
         var sourceRepository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
-        var searchResource = await sourceRepository.GetResourceAsync<PackageSearchResource>();
         var downloadResource = await sourceRepository.GetResourceAsync<DownloadResource>();
 
-        string searchTerm = "microsoft";
-        int maxResults = 1000;
+        HttpClient client = new HttpClient();
+        client.Timeout = TimeSpan.FromMinutes(5);
+        client.DefaultRequestHeaders.UserAgent.ParseAdd("nuget-mirror/1.0");
 
-        var searchResults = await searchResource.SearchAsync(
-            searchTerm,
-            new SearchFilter(includePrerelease: true),
-            skip: 0,
-            take: maxResults,
-            NullLogger.Instance,
-            CancellationToken.None);
-
-        foreach (var package in searchResults)
+        string[] searchTerms = new string[]
         {
-            var existingDirectory =
-                Directory.GetDirectories("D:\\Neximus.DownloadNugetPackages\\Neximus.DownloadNugetAllPackages\\Neximus.DownloadNugetAllPackages\\bin\\Debug\\net8.0\\DownloadedPackages");
+            "microsoft",
+            //"system",
+            //"extensions",
+            //"aspnet",
+            //"entityframework",
+            //"json",
+            //"logging",
+            //"http",
+            //"security",
+            //"identity",
+            //"newtonsoft",
+            //"serilog",
+            //"dapper",
+            //"automapper",
+            //"mediatr",
+            //"polly",
+            //"blazor",
+            //"xunit"
+        };
 
-            if (existingDirectory.Contains(package.Identity.Id.ToLower()))
-                return;
+        int maxResults = 1000;
+        long minDownloads = 100_000;
+        int batchSize = 200; // برای جلوگیری از JSON بزرگ
 
-            var versions = await package.GetVersionsAsync();
+        foreach (var term in searchTerms)
+        {
+            Console.WriteLine($"\n=== Searching '{term}' ===");
 
-            var supportedVersions = versions
-                .ToList()
-                .GroupBy(_ => _.Version.Major)
-                .Select(_ => _.Last());
+            int skip = 0;
+            int totalFetched = 0;
 
-            if (supportedVersions.Any())
+            while (totalFetched < maxResults)
             {
-                string packageFolder = Path.Combine(rootFolder, package.Identity.Id.ToLower());
-                Directory.CreateDirectory(packageFolder);
+                string url = $"https://api-v2v3search-0.nuget.org/query?q={term}&prerelease=true&take={batchSize}&skip={skip}&sortBy=downloads";
 
-                foreach (var version in supportedVersions)
+                using var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                response.EnsureSuccessStatusCode();
+
+                using var stream = await response.Content.ReadAsStreamAsync();
+                using var doc = await JsonDocument.ParseAsync(stream);
+
+                var packages = doc.RootElement.GetProperty("data").EnumerateArray();
+
+                int fetchedInThisBatch = 0;
+
+                foreach (var pkg in packages)
                 {
-                    Console.WriteLine($"Downloading {package.Identity.Id} - Version: {version.Version}");
+                    string packageId = pkg.GetProperty("id").GetString();
+                    long downloadCount = pkg.GetProperty("totalDownloads").GetInt64();
 
-                    var packageIdentity = new NuGet.Packaging.Core.PackageIdentity(package.Identity.Id, version.Version);
+                    if (downloadCount < minDownloads)
+                        continue;
 
-                    var downloadResult = await downloadResource.GetDownloadResourceResultAsync(
-                        packageIdentity,
-                        new PackageDownloadContext(new SourceCacheContext()),
-                        SettingsUtility.GetGlobalPackagesFolder(Settings.LoadDefaultSettings(null)),
-                        NullLogger.Instance,
-                        CancellationToken.None);
-
-                    if (downloadResult.Status == DownloadResourceResultStatus.Available)
+                    string packageFolder = Path.Combine(rootFolder, packageId.ToLowerInvariant());
+                    if (!Directory.Exists(packageFolder))
                     {
-                        string packageFileName = $"{package.Identity.Id}.{version.Version}.nupkg";
-                        string packagePath = Path.Combine(packageFolder, packageFileName);
+                        Directory.CreateDirectory(packageFolder);
 
-                        using (var fileStream = File.Create(packagePath))
+                        var versionsJson = pkg.GetProperty("versions");
+                        var selectedVersions = versionsJson.EnumerateArray()
+                            .GroupBy(v => v.GetProperty("version").GetString().Split('.')[0])
+                            .Select(g => g.OrderByDescending(v => NuGetVersion.Parse(v.GetProperty("version").GetString())).First());
+
+                        foreach (var v in selectedVersions)
                         {
-                            await downloadResult.PackageStream.CopyToAsync(fileStream);
-                        }
+                            string versionStr = v.GetProperty("version").GetString();
+                            try
+                            {
+                                Console.WriteLine($"Downloading {packageId} - {versionStr} - Downloads: {downloadCount}");
 
-                        Console.WriteLine($"Downloaded {packageFileName} to {packageFolder}");
+                                var identity = new PackageIdentity(packageId, NuGetVersion.Parse(versionStr));
+
+                                var result = await downloadResource.GetDownloadResourceResultAsync(
+                                    identity,
+                                    new PackageDownloadContext(new SourceCacheContext()),
+                                    SettingsUtility.GetGlobalPackagesFolder(Settings.LoadDefaultSettings(null)),
+                                    NullLogger.Instance,
+                                    CancellationToken.None);
+
+                                if (result.Status == DownloadResourceResultStatus.Available)
+                                {
+                                    string fileName = $"{identity.Id}.{identity.Version}.nupkg";
+                                    string filePath = Path.Combine(packageFolder, fileName);
+
+                                    using var fs = File.Create(filePath);
+                                    await result.PackageStream.CopyToAsync(fs);
+
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine($"Downloaded {fileName}");
+                                    Console.ResetColor();
+                                }
+                            }
+                            catch
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine($"Failed: {packageId} - {versionStr}");
+                                Console.ResetColor();
+                            }
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine($"Failed to download {package.Identity.Id} - Version: {version.Version}");
-                    }
+                    fetchedInThisBatch++;
+                    totalFetched++;
+                    if (totalFetched >= maxResults)
+                        break;
                 }
+
+                if (fetchedInThisBatch == 0)
+                    break; // دیگر بسته‌ای برای دریافت نیست
+
+                skip += batchSize;
             }
+
+            Console.WriteLine($"\nFinished search term '{term}'");
         }
 
-        Console.WriteLine("Done!");
+        Console.WriteLine("\nAll done!");
     }
 }
